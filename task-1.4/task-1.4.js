ba3f3c8ceda06db59e21a9acc8c2cb05b3e0daa6
Bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-04-05 15:18:03 +0200

    add index.html + task-1.4.js

---
+++ task-1.4/task-1.4.js
@@ -0,0 +1,11 @@
+var name = "bartosz"
+var age$ = 27
+var humorToday = "5.04.2018"
+var dayToday = "Thursday"
+var Random_Numbers = [1, 2, 3, 4, 5]
+
+console.log('name', name, typeof name)
+console.log('age', age$, typeof age$)
+console.log('humor today', humorToday, typeof humorToday)
+console.log('day today', dayToday, typeof dayToday)
+console.log('random numbers', Random_Numbers, typeof Random_Numbers)
